db9a12554e176b0c59e9bc66d602d6b635c9e67d
nava6x <navafacts2026@gmail.com>
2025-03-25 17:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(
     saveUninitialized: false,
     rolling: true, // Reset maxAge on every request
     cookie: {
-      httpOnly: true,
+      httpsOnly: true,
       secure: true,
       sameSite: 'strict',
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
