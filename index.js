851b0570388132753ace818770d94893a1e7bfcf
nava6x <navafacts2026@gmail.com>
2025-03-26 12:44:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(express.json());
 app.use(cors({
     origin: ["https://tst-frontend.onrender.com", "http://localhost:3000"],
     methods: ["GET", "POST"]
