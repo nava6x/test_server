55dc80e148311a133a7b3a52f916cfe77534c780
nava6x <navafacts2026@gmail.com>
2025-03-26 13:13:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get("/", (req, res) => {
     res.send(`Your IP Address is: ${forwardedIp || ip}`);
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
